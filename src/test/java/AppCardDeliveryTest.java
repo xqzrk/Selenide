2e5d74e9d9d258148a5ceee20c39694c5811520d
MSevryukov <smlog@yandex.ru>
2023-04-17 22:12:34 +0300

    Homework v.2

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,91 +1,84 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
-
 public class AppCardDeliveryTest {
 
-    public String meet() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_MONTH, 3);
-        Date date = calendar.getTime();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        String meetDate = formatter.format(date);
-        return meetDate;
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    public int meetDate() {
-        Calendar calendar2 = new GregorianCalendar();
-        calendar2.add(Calendar.DAY_OF_MONTH, 7);
-        int dayOfWeek = calendar2.get(Calendar.DAY_OF_WEEK);
-        if (dayOfWeek > 6 & dayOfWeek < 8) {
-            calendar2.add(Calendar.DAY_OF_MONTH, 2);
-        }
-        if (dayOfWeek > 0 & dayOfWeek < 2) {
-            calendar2.add(Calendar.DAY_OF_MONTH, 1);
-        }
-        int meetDate = calendar2.get(Calendar.DATE);
-        return meetDate;
+    @Test
+    public void shouldTest() {
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//span[@data-test-id='date']//input").setValue(planningDate);
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Петровский Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79998887766");
+        $x("//span[@class='checkbox__box']").click();
+        $x("//span[contains(text(), 'Забронировать')]").click();
+        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 
-    public int meetMonth() {
-        Calendar calendar2 = new GregorianCalendar();
-        calendar2.add(Calendar.DAY_OF_MONTH, 7);
-        int dayOfWeek = calendar2.get(Calendar.DAY_OF_WEEK);
-        if (dayOfWeek > 6 & dayOfWeek < 8) {
-            calendar2.add(Calendar.DAY_OF_MONTH, 2);
+    public int dayOfMeet(int addDays) {
+        int potentialDay = LocalDate.now().plusDays(addDays).getDayOfMonth();
+        if (LocalDate.now().plusDays(addDays).getDayOfWeek() == DayOfWeek.SATURDAY) {
+            potentialDay = potentialDay + 2;
         }
-        if (dayOfWeek > 0 & dayOfWeek < 2) {
-            calendar2.add(Calendar.DAY_OF_MONTH, 1);
+        if (LocalDate.now().plusDays(addDays).getDayOfWeek() == DayOfWeek.SUNDAY) {
+            potentialDay = potentialDay + 1;
         }
-        return calendar2.get(Calendar.MONTH);
+        return potentialDay;
     }
 
-    public boolean monthComparison() {
-        Calendar actual = new GregorianCalendar();
-        int actualMonth = actual.get(Calendar.MONTH);
-        if (actualMonth < meetMonth()) {
+    public int meetMonthV2(int addDays) {
+        return LocalDate.now().plusDays(addDays).getMonthValue();
+    }
+
+    public boolean monthCheck() {
+        int currentMonth = LocalDate.now().getMonthValue();
+        if (currentMonth < meetMonthV2(7)) {
             return true;
         } else {
             return false;
         }
     }
 
+    public String dateOfMeet(int addDays, String pattern) {
+        int addDaysPlus = addDays;
 
-    @Test
-    public void shouldTest() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
-        $x("//span[@data-test-id='city']//input").setValue("Москва");
-        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $x("//span[@data-test-id='date']//input").setValue(meet());
-        $x("//span[@data-test-id='name']//input").setValue("Иванов-Петровский Иван");
-        $x("//span[@data-test-id='phone']//input").setValue("+79998887766");
-        $x("//span[@class='checkbox__box']").click();
-        $x("//span[contains(text(), 'Забронировать')]").click();
-        $x("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        if (LocalDate.now().plusDays(addDays).getDayOfWeek() == DayOfWeek.SATURDAY) {
+            addDaysPlus = addDays + 2;
+        }
+        if (LocalDate.now().plusDays(addDays).getDayOfWeek() == DayOfWeek.SUNDAY) {
+            addDaysPlus = addDays + 1;
+        }
+        return LocalDate.now().plusDays(addDaysPlus).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
     public void shouldTestV2() {
-        int date = meetDate();
+        String planningDate = dateOfMeet(7, "dd.MM.yyyy");
+        int date = dayOfMeet(7);
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $x("//span[@data-test-id='city']//input").setValue("Мо");
         $x("//span[contains(text(), 'Москва')]").click();
         $x("//span[@class='input__icon']").click();
-        if (monthComparison() == true) {
+        if (monthCheck() == true) {
             $x("//div[@data-step='1']").click();
             $x("//td[contains(text(), '" + date + "')]").click();
         } else {
@@ -95,6 +88,6 @@ public class AppCardDeliveryTest {
         $x("//span[@data-test-id='phone']//input").setValue("+79998887766");
         $x("//span[@class='checkbox__box']").click();
         $x("//span[contains(text(), 'Забронировать')]").click();
-        $x("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
